cbfeeee314b848c999e940216325a8e6e29434c6
Dylan Peerenboom & Jeremy Whitaker <student@epicodus.com>
2014-07-17 14:12:14 -0700

    Modified object name for better OO handling. Tests pass

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,9 @@
-var ShippingCalc = {
+var Package = {
   classMultiplier: 1,
   mass: 1,
   volume: 1,
-  distance:0,
-  shippingClass: function(classSelect){
+  distance: 0,
+  packageClass: function(classSelect){
     if (classSelect === 'Slip Stream') {
       this.classMultiplier = 3;
     } else if (classSelect === 'Warp Speed'){
@@ -12,21 +12,25 @@ var ShippingCalc = {
       this.classMultiplier = 1;
     } else alert("You've broken the hyperdrive!");
   },
-  shippingWeight: function(weight){
+  packageWeight: function(weight){
     this.mass = this.mass * weight;
   },
-  shippingDimensions: function(length, width, height){
+  packageDimensions: function(length, width, height){
     this.volume = length * width * height;
   },
-  shippingDistance: function(from,to){
+  packageDistance: function(from,to){
+    alpha = -1000,
+    beta = 2000,
+    delta = 10000,
+    gamma = -5000,
+    transdimensional = Math.pow(10,10),
     this.distance = Math.abs(to-from);
   },
-  cost: this.classMultiplier*this.mass*this.volume*this.distance,
 };
 
-// var Quadrants = {
-//   name: '',
-//   distanceFromBase: 1,
-
-// }
-
+var myPackage = Object.create(Package);
+myPackage.packageClass('Slip Stream');
+myPackage.packageWeight(10);
+myPackage.packageDimensions(5,5,5);
+myPackage.packageDistance(100,-100);
+console.log(myPackage.packageDistance(100,-110));
--- spec/specs.js
+++ spec/specs.js
@@ -1,30 +1,30 @@
-describe("ShippingCalc", function(){
-  describe("shippingClass", function(){
+describe("Package", function(){
+  describe("packageClass", function(){
     it("method sets a class multiplier base on shipping type selected", function(){
-      var testShippingCalc = Object.create(ShippingCalc);
-      testShippingCalc.shippingClass('Slip Stream');
-      testShippingCalc.classMultiplier.should.equal(3);
+      var testPackage = Object.create(Package);
+      testPackage.packageClass('Slip Stream');
+      testPackage.classMultiplier.should.equal(3);
     });
   });
-  describe("shippingWeight", function(){
+  describe("packageWeight", function(){
     it("method to store weight for later multiplication", function(){
-      var testShippingCalc = Object.create(ShippingCalc);
-      testShippingCalc.shippingWeight(50);
-      testShippingCalc.mass.should.equal(50);
+      var testPackage = Object.create(Package);
+      testPackage.packageWeight(50);
+      testPackage.mass.should.equal(50);
     });
   });
-    describe("shippingDimensions", function(){
+    describe("packageDimensions", function(){
     it("method to store weight for later multiplication", function(){
-      var testShippingCalc = Object.create(ShippingCalc);
-      testShippingCalc.shippingDimensions(5,5,5);
-      testShippingCalc.volume.should.equal(125);
+      var testPackage = Object.create(Package);
+      testPackage.packageDimensions(5,5,5);
+      testPackage.volume.should.equal(125);
     });
   });
-    describe("shippingDistance", function(){
+    describe("packageDistance", function(){
     it("method to determine distance between two planetary systems", function(){
-      var testShippingCalc = Object.create(ShippingCalc);
-      testShippingCalc.shippingDistance(-5000,2600);
-      testShippingCalc.distance.should.equal(7600);
+      var testPackage = Object.create(Package);
+      testPackage.packageDistance(alpha , transdimensional);
+      testPackage.distance.should.equal(10000001000);
     });
   });
 });
